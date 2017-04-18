e218eb55c3df5b5fd66654393b8461e2ff3f0252
Arthur Carabott <arthurc@gmail.com>
2017-04-18 08:41:53 +0100

    drawing circle

--- js/touch-distance.js
+++ js/touch-distance.js
@@ -11,10 +11,25 @@ class TouchDistance {
     this.min = 0.0;
     this.max = 1.0;
     this.value = 0.5;
+
+    this.render();
   }
 
   appendTo(domElement) {
     domElement.appendChild(this.canvas);
+  }
+
+  render() {
+    this.ctx.save();
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.ctx.fillStyle = 'rgba(43, 156, 212, 1.0)';
+
+    this.ctx.beginPath();
+    this.ctx.arc(50, 50, 50, 0, Math.PI * 2, false);
+    this.ctx.fill();
+
+    this.ctx.restore();
   }
 }
 
